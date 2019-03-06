cdc98a99bd6c718065e2aff3b52ef0364d05f1be
Shitong Zhu <shitong.zhu@email.ucr.edu>
2019-03-06 10:29:45 -0800

    Init code release for ShadowBlock (v1.0-alpha).

--- README.md
+++ README.md
@@ -1,6 +1,61 @@
 # ShadowBlock
-We are making some final adjustments to our code. As soon as everything is finalized, we will release both the source code and a working binary to allow reproducibility as well as facilitate future extensions. Stay tuned!
+ShadowBlock is a modified Chromium browser that hides ads in a stealthy manner so it won't trigger any anti-adblocking response from websites, at rather low runtime overhead. Evaluations shows it achieves 100% evasion success rate on a small-scale anti-adblocker dataset and over 98% ad coverage among Alexa Top-1000 websites. At the same time, its average performance (in terms of both Page Load Time and SpeedIndex) is on par with stock Chromium running Adblock Plus. Kindly refer to our [WWW paper](#Reference) for more details.
 
+**Note we have only tested ShadowBlock on Manjora Linux (KDE) 18.0.3 and Ubuntu 16.04, but it should be compatible with other distributions with only minor adjustments**
+
+Feel free to contact [Shitong Zhu](mailto:shitong.zhu@email.ucr.edu) or open GitHub issues if you have any problem running/building ShadowBlock. 
+
+## Outline
+```
+├── bin
+│   ├── shadowblock_css_socket_server
+│   └── ShadowBlock-Release.tar.gz
+├── LICENSE
+├── patches
+│   ├── patch.diff
+│   └── v8_patch.diff
+├── README.md
+└── src
+    └── shadowblock_css_socket_server.cpp
+```
+
+We provide both the binaries (**download them from GitHub's "Releases" tab**) and source code (as Chromium patches) to allow for reproducibility and future extensions from research commiunity.
+
+## Run
+Binaries are located in the release folder. Please follow the steps below to run it.
+1. Unzip the folder `ShadowBlock-Release` from `ShadowBlock-Release.tar.gz`;
+2. Run the standalone socket server to supply with site-specific CSS HTML rules by `./shadowblock_css_socket_server`;
+3. Open another terminal and start Chromium with a fresh user profile by `ShadowBlock-Release/chrome --no-sandbox --user-data-dir=/PATH/TO/NEW/PROFILE`. Note you need to wait for several minutes for Chromium to automatically download the EasyList that will be used by ShadowBlock;
+4. Once the `Indexed Rules` folder is generated under the `Subreousrce Filter` folder in user profile (i.e. `/PATH/TO/NEW/PROFILE` in Step 4), it means the EasyList is already in place and you are ready to browse. Note this only needs to be done once as the downloaded ruleset persists in the profile;
+5. Restart Chromium and browse normally. Make sure the `shadowblock_css_socket_server` is running in background.
+
+## Build from scratch
+Our source code is provided as Chromium patches. Follow these steps to apply and integrate them into stock Chromium.
+1. Check out Chromium code from [this tutorial](https://www.chromium.org/developers/how-tos/get-the-code);
+2. `git checkout` the main **`src/`** repo to commit `2f76aeef54efba5408522beb7b1dd14973c9338b` and apply our patch by `git apply /PATH/TO/patches/patch.diff`;
+3. `git checkout` the **`v8/`** repo to commit `c670249b587112de339a8fa49e7cea13f8f559b4` and apply our patch by `git apply /PATH/TO/patches/v8_patch.diff`;
+4. Set build parameters as below (by `gn args /PATH/TO/CHROMIUM/BUILD`):
+```
+# You may adjust this as needed, but 
+# "use_jumbo_build = true" is required 
+# as per our namespace usage
+is_debug = false
+symbol_level = 0
+remove_webcore_debug_symbols = true
+is_official_build = true
+use_jumbo_build = true
+enable_nacl = false
+```
+5. `autoninja -C out/ShadowBlock-Release` and you will get the binary in `out/ShadowBlock-Release` that is essentially as same as the one provided by us. Now if you choose to use existing `shadowblock_css_socket_server` executable, you can move to the **"Run"** section above to start using it.
+
+If you prefer building `shadowblock_css_socket_server` from scratch as well, follow these steps further:
+1. Clone the repo of [**libadblockplus**](https://github.com/adblockplus/libadblockplus);
+2. `./ensure_dependencies.py` to install necessary dependencies;
+3. `make get-prebuilt-v8` to download the v8 library `libv8_monolith.a`;
+4. `make` to build the library `libadblockplus.a`;
+5. Copy `shadowblock_css_socket_server.cpp` to directory `libadblockplus`, and build executable `shadowblock_css_socket_server` by `clang++ -stdlib=libc++ -std=c++14 -fuse-ld=lld -Iinclude -Ithird_party/prebuilt-v8/include shadowblock_css_socket_server.cpp -o shadowblock_css_socket_server -L./ -ladblockplus -lv8_monolith -lpthread -lcurl`.
+
+## Reference
 Check our WWW '19 paper for more architectural and technical details:
 
 Shitong Zhu, Umar Iqbal, Zhongjie Wang, Zhiyun Qian, Zubair Shafiq, and Weiteng Chen. 2019. **ShadowBlock: A Lightweight and Stealthy Adblocking Browser**. In *Proceedings of the 2019 World Wide Web Conference (WWW’19), May 13–17, 2019, San Francisco, CA, USA*. ACM, New York, NY, USA, 11 pages. https://doi.org/10.1145/3308558.3313558
---
+++ src/shadowblock_css_socket_server.cpp
@@ -0,0 +1,235 @@
+// ShadowBlock project
+// UCR Security Lab/UIowa IRL 2019
+// @author Shitong Zhu
+// https://github.com/seclab-ucr/ShadowBlock
+
+#include "include/AdblockPlus.h"
+#include "include/AdblockPlus/Platform.h"
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <set>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+
+#define TRUE 1
+#define FALSE 0
+// You may change this, but need to make it consistent with
+// the port used in Chromium
+#define SERVER_PORT 8888
+// Large enough to contain vert long CSS selectors
+#define SELECTOR_STRING_OVERALL_BUFFER_SIZE 1024000
+
+using namespace AdblockPlus;
+
+const std::string MergeAllSelectors(std::vector<std::string> vec) {
+  std::string res = "";
+  for (int i = 0; i < vec.size() - 1; i++) {
+    res.append(vec[i]);
+    res.append(",");
+  }
+  res.append(vec[vec.size() - 1]);
+  return res;
+}
+
+int main() {
+  AdblockPlus::AppInfo appInfo;
+  appInfo.version = "1.0";
+  appInfo.name = "elemhidingremoteshell";
+  appInfo.application = "standalone";
+  appInfo.applicationVersion = "1.0";
+  appInfo.locale = "en-US";
+
+  auto platform = DefaultPlatformBuilder().CreatePlatform();
+  platform->SetUpJsEngine(appInfo);
+  JsEngine& jsEngine = platform->GetJsEngine();
+  auto& filterEngine = platform->GetFilterEngine();
+
+  int opt = TRUE;
+  int master_socket, addrlen, new_socket, client_socket[300],
+      max_clients = 300, activity, i, valread, sd;
+  int max_sd;
+  struct sockaddr_in address;
+
+  std::cout << "[INFO] Launching the HTML rule module of ShadowBlock\n"
+	    << std::endl;
+
+  // Data buffer of 1K
+  char buffer[SELECTOR_STRING_OVERALL_BUFFER_SIZE];
+
+  // Set of socket descriptors
+  fd_set readfds;
+
+  // Initialize all client_socket[] to 0 so not checked
+  for (i = 0; i < max_clients; i++) {
+    client_socket[i] = 0;
+  }
+
+  // Create a master socket
+  if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Set master socket to allow multiple connections ,
+  if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char*)&opt,
+                 sizeof(opt)) < 0) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
+
+  // Type of socket created
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(SERVER_PORT);
+
+  // Bind the socket to localhost port 8888
+  if (bind(master_socket, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  printf("[INFO] Listening on port %d\n", SERVER_PORT);
+
+  // try to specify maximum of 3 pending connections for the master socket
+  if (listen(master_socket, 3) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  // accept the incoming connection
+  addrlen = sizeof(address);
+  puts("[INFO] Waiting for connections");
+
+  while (TRUE) {
+    // Clear the socket set
+    FD_ZERO(&readfds);
+
+    // Add master socket to set
+    FD_SET(master_socket, &readfds);
+    max_sd = master_socket;
+
+    // Add child sockets to set
+    for (i = 0; i < max_clients; i++) {
+      // Socket descriptor
+      sd = client_socket[i];
+
+      // If it's valid socket descriptor then add to read list
+      if (sd > 0)
+        FD_SET(sd, &readfds);
+
+      // Highest file descriptor number, need it for the select function
+      if (sd > max_sd)
+        max_sd = sd;
+    }
+
+    // Waiting for an activity on one of the sockets
+    // Timeout being NULL means indefinitely
+    activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+    if ((activity < 0) && (errno != EINTR)) {
+      printf("[ERROR] Select error");
+    }
+
+    // If something happened on the master socket,
+    // then its an incoming connection
+    if (FD_ISSET(master_socket, &readfds)) {
+      if ((new_socket = accept(master_socket, (struct sockaddr*)&address,
+                               (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+      }
+
+      // Inform user of socket number
+      printf("[INFO] New incoming connection\n");
+
+      // Add new socket to array of sockets
+      for (i = 0; i < max_clients; i++) {
+        // If position is empty
+        if (client_socket[i] == 0) {
+          client_socket[i] = new_socket;
+          printf("[INFO] Adding to list of sockets\n");
+          break;
+        }
+      }
+    }
+
+    // Else its some IO operation on some other socket
+    for (i = 0; i < max_clients; i++) {
+      sd = client_socket[i];
+
+      if (FD_ISSET(sd, &readfds)) {
+        // Check if it was for closing, and also read the
+        // incoming message
+        if ((valread = read(sd, buffer, SELECTOR_STRING_OVERALL_BUFFER_SIZE)) <=
+            0) {
+          // One client is being disconnected
+          getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+          printf("[INFO] Host disconnected, IP %s, PORT %d \n",
+                 inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+          // Close the socket and mark as 0 in list for reuse
+          close(sd);
+          client_socket[i] = 0;
+        }
+
+        // Echo back the message that came in
+        else {
+          std::cout << "[INFO] Length of msg: " << valread << "\n";
+
+          buffer[valread] = '\0';
+          std::vector<std::string> domain_selectors;
+          std::vector<std::string> generic_selectors;
+
+          std::string tab_url(buffer);
+          std::cout << "[INFO] URL: " << tab_url << "\n";
+          std::string host_domain = filterEngine.GetHostFromURL(tab_url);
+
+          domain_selectors =
+              filterEngine.GetElementHidingSelectors(host_domain);
+          generic_selectors = filterEngine.GetElementHidingSelectors("");
+
+          std::cout << "[INFO] Size of domain selectors: "
+		    << domain_selectors.size() 
+		    << std::endl;
+          std::cout << "[INFO] Size of generic selectors: "
+		    << generic_selectors.size() 
+		    << std::endl;
+
+          std::set<std::string> domain_selectors_set(domain_selectors.begin(),
+                                                domain_selectors.end());
+          std::set<std::string> generic_selectors_set(generic_selectors.begin(),
+                                                generic_selectors.end());
+          generic_selectors_set.insert(domain_selectors_set.begin(),
+                                 domain_selectors_set.end());
+          std::vector<std::string> selectors_final(generic_selectors_set.begin(),
+                                                   generic_selectors_set.end());
+          std::cout << "[INFO] Size of final selectors: "
+		    << selectors_final.size() 
+		    << std::endl;
+          std::string res_str = MergeAllSelectors(selectors_final);
+          const char* res_c_str = res_str.c_str();
+
+          send(sd, res_c_str, strlen(res_c_str), 0);
+          std::cout << "[INFO] Selector string sent" << std::endl;
+          const char* signal_to_shutdown = ",.NOTVERYUNIQUESTRING";
+          send(sd, signal_to_shutdown, strlen(signal_to_shutdown), 0);
+          std::cout << "[INFO] Stop signal sent\n" << std::endl;
+          close(sd);
+          client_socket[i] = 0;
+        }
+      }
+    }
+  }
+
+  return 0;
+}
